d1d734dfcb96e5567cde5e973c358f1c51207614
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-02-26 06:01:04 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-tf"
+    bucket = "terraform-state-bucket-tf-app"
     key    = "terraform.tfstate"
     region = "eu-central-1"
   }
