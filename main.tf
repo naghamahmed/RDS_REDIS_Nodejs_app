a01d316acffb73f4104e5f3c663a7a60f89262a5
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-02-26 05:34:23 +0200

    update database module

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ module "network" {
   availability_zone_2        = var.availability_zone_2
 }
 
-# module "database" {
-#   source = "./modules/Database"
-#   SECURITY_GROUP_ID = module.network.SG_ID
-#   RDS_USERNAME      = var.RDS_USERNAME
-#   AZ                = var.availability_zone_1
-#   DB_SUBNET_1       = module.network.private_subnet1_id
-#   DB_SUBNET_2       = module.network.private_subnet2_id 
-#   VPC_ID            = module.network.vpc_id
-#   ec2_sg            = module.network.sg_2
-# }
+module "database" {
+  source = "./modules/Database"
+  SECURITY_GROUP_ID = module.network.SG_ID
+  RDS_USERNAME      = var.RDS_USERNAME
+  AZ                = var.availability_zone_1
+  DB_SUBNET_1       = module.network.private_subnet1_id
+  DB_SUBNET_2       = module.network.private_subnet2_id 
+  VPC_ID            = module.network.vpc_id
+  ec2_sg            = module.network.sg_2
+}
