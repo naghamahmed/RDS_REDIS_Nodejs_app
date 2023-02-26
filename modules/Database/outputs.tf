88fa1965ccdc0c84439ee3fdf80d92f24bbbcb0a
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-02-26 05:01:13 +0200

    Update outputs.tf

--- modules/Database/outputs.tf
+++ modules/Database/outputs.tf
@@ -1,11 +1,11 @@
-# output "RDS_HOSTNAME" {
-#     value = aws_db_instance.rds.address
-# }
+output "RDS_HOSTNAME" {
+    value = aws_db_instance.rds.address
+}
 
-# output "REDIS_HOSTNAME"{
-#     value = aws_elasticache_cluster.elasticashe.cache_nodes.0.address
-# }
+output "REDIS_HOSTNAME"{
+    value = aws_elasticache_cluster.elasticashe.cache_nodes.0.address
+}
 
-# output "RDS_PASSWORD" {
-#     value = random_password.password.result
-# }
+output "RDS_PASSWORD" {
+    value = random_password.password.result
+}
