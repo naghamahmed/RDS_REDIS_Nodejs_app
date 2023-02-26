7d2c98680d895dc1f398d95a508633263bd40b30
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-02-26 05:01:38 +0200

    Update variables.tf

--- modules/Database/variables.tf
+++ modules/Database/variables.tf
@@ -1,29 +1,29 @@
 
-# variable "SECURITY_GROUP_ID" {
-#   type        = string
-# }
+variable "SECURITY_GROUP_ID" {
+  type        = string
+}
 
-# variable "RDS_USERNAME" {
-#   type = string
-# }
+variable "RDS_USERNAME" {
+  type = string
+}
 
-# variable "AZ" {
-#   type = string
-# }
+variable "AZ" {
+  type = string
+}
 
-# variable "DB_SUBNET_1" {
-#   type = string
-# }
+variable "DB_SUBNET_1" {
+  type = string
+}
 
-# variable "DB_SUBNET_2" {
-#   type = string
-# }
+variable "DB_SUBNET_2" {
+  type = string
+}
 
-# variable "VPC_ID" {
-#   type = string
-# }
+variable "VPC_ID" {
+  type = string
+}
 
-# variable "ec2_sg" {
-#   type = string
-# }
+variable "ec2_sg" {
+  type = string
+}
 
