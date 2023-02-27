d32b63c9233d780c37319a1febb2872ded99b4b6
naghamahmed <110065223+naghamahmed@users.noreply.github.com>
2023-02-27 09:20:37 +0200

    Update key-pair.tf

--- key-pair.tf
+++ key-pair.tf
@@ -15,6 +15,22 @@
   # provisioner "local-exec" {
   #   command = "chmod +x ~/.ssh/key_pair.pem"
   # }
-
-
  }
+
+resource "aws_secretsmanager_secret" "private_key" {
+  name = "private_key"
+}
+
+resource "aws_secretsmanager_secret" "public_key" {
+  name = "public_key"
+}
+
+resource "aws_secretsmanager_secret_version" "private_key_value" {
+  secret_id     = aws_secretsmanager_secret.private_key.id
+  secret_string = tls_private_key.pk.private_key_pem
+}
+
+resource "aws_secretsmanager_secret_version" "public_key_value" {
+  secret_id     = aws_secretsmanager_secret.public_key.id
+  secret_string = aws_key_pair.key.public_key
+}
